8d914b68599d44228fdeb19a09ff6a73b0d9b53a
kesllan <kesllan@gmail.com>
2023-02-09 23:18:08 -0300

    add-tawk.to

--- resources/views/layout/public.blade.php
+++ resources/views/layout/public.blade.php
@@ -93,6 +93,25 @@
         @endif
     </div>
 </div>
+</script>
 
+@include('layout.vue')
+
+@stack('scripts')
+
+@stack('end')
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6359f652b0d6371309cbc27d/1ggbmkcuo';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
